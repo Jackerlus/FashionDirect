1d8c30a0e6695ea9e40428fb5da653f7690d1b34
Jack Lewis <jack.lewis159@gmail.com>
2018-04-09 00:48:27 +0100

    Added 'contact us' page

---
+++ contact.php
@@ -0,0 +1,9 @@
+<?php
+//These statements are used to call in the various modules that make up a webpage
+include("header.php");
+include("navbar.php");
+?>
+<h1>Contact Us</h1>
+<?php
+include("footer.php");
+?>
