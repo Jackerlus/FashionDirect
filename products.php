3f4c85beca968d3cb898c557ef0130aeff6cdbbe
Jack Lewis <jack.lewis159@gmail.com>
2018-04-09 13:51:22 +0100

    Added a products page to display items

--- products.php
+++ products.php
@@ -1,15 +1,39 @@
-<?php
-$connect = mysqli_connect('localhost', 'root', '', 'fd');
-$query = 'SELECT * FROM products ORDER by id ASC';
-$result = mysqli_query($connect, $query);
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>All Products</title>
+        <link href="css/products.css" rel="stylesheet" type="text/css"/>
+        <?php
+        include("header.php");
+        ?>
+    </head>
+    <body>
+        <div class="container-fluid">
+            <div class="row justify-content-center">
+    <?php
+    $connect = mysqli_connect('localhost', 'root', '', 'fd');
+    $query = 'SELECT * FROM products ORDER by id ASC';
+    $result = mysqli_query($connect, $query);
 
-if ($result)
-{
-    if(mysqli_num_rows($result) > 0)
+    if ($result)
     {
-        while($product = mysqli_fetch_assoc($result))
+        if(mysqli_num_rows($result) > 0)
         {
-            print_r($product);
+            while($product = mysqli_fetch_assoc($result))
+            { ?>
+                <div class="col-sm-4">
+                    <div style="" class="product-card">
+                        <img id="product-image" src="<?php echo $product['image'];?>" class="img-responsive" />
+                        <a id="product-name" class="text-info" href="item.php?id=<?php echo $product['id']; ?>"><?php echo $product['name']; ?></a>
+                        <h4 class="">$ <?php echo $product['price']; ?></h4>
+                    </div>
+                </div>
+            <?php
+            }
         }
     }
-}
+    ?>
+            </div>
+        </div>
+    </body>
+</html>
