80ec187021a80642bbe5e7f01cc102865366f036
Jack Lewis <jack.lewis159@gmail.com>
2018-04-09 13:51:35 +0100

    Made minor changes to the header

--- header.php
+++ header.php
@@ -1,9 +1,3 @@
 <!-- The header for each web page -->
 <link href="css/header.css" rel="stylesheet" type="text/css"/>
 <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
-<html>
-    <head>
-        <title>Fashion Direct - Home</title>
-    </head>
-    <body>
-    </body>
