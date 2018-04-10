08f87baa9d99c082d1b3629f27f27595ac4d12ab
Jack Lewis <jack.lewis159@gmail.com>
2018-04-10 01:44:08 +0100

    Added functionality to the item page and added device responsiveness
    The detailed item page now displays all relevant information about a specific item

--- item.php
+++ item.php
@@ -1,3 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>All Products</title>
+    <link href="css/item.css" rel="stylesheet" type="text/css"/>
+    <?php
+    include("header.php");
+    ?>
+</head>
+<body>
 <?php
-echo $_GET['id'];
+include("navbar.php");
+
+$selectedItem = $_GET['id'];
+$connect = mysqli_connect('localhost', 'root', '', 'fd');
+$query = "SELECT * from products WHERE id = '$selectedItem'";
+$result = mysqli_query($connect, $query);
+$product = mysqli_fetch_assoc($result);
+
+if ($result)
+{
+    if(mysqli_num_rows($result) > 0)
+    {
+        ?>
+        <div class="product-card">
+            <img id="product-image" src="<?php echo $product['image'];?>" class="img-responsive" />
+            <h3 id="product-name" class="text-info"><?php echo $product['name']; ?></h3>
+            <h4>£<?php echo $product['price']; ?></h4>
+            <p><?php echo $product['description']; ?></p>
+        </div>
+        <?php
+        }
+    }
 ?>
