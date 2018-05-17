da962ef898b5c59e7700d9f3be9d06f2ed7d9841
Jack Lewis <jack.lewis159@gmail.com>
2018-05-17 23:05:52 +0100

    Minor changes to header

--- header.php
+++ header.php
@@ -1,3 +1,6 @@
 <!-- The header for each web page -->
 <link href="css/header.css" rel="stylesheet" type="text/css"/>
 <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+<?php
+session_start();
+?>
