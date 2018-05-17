f17570f4485c7839c84e464a9f71e7e026aace4c
Jack Lewis <jack.lewis159@gmail.com>
2018-05-18 00:16:10 +0100

    Started customer details input checkout page

---
+++ custDetails.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+    <?php
+    include("header.php");
+    include("navbar.php");
+    ?>
+</head>
+<body>
+    <form action="submitAddress.php" id="custAddress" method="post"
+</body>
+<?php
+include("footer.php");
+?>
+</html>
